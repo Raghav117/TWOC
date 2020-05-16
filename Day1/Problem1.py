06f7e504b1868b7041f80a1517c739b67d12cb69
Raghav Garg <42090624+Raghav117@users.noreply.github.com>
2020-05-16 23:09:50 +0530

    Add files via upload

---
+++ Day1/Problem1.py
@@ -0,0 +1,7 @@
+Name = input("Your Name ")
+Branch = input("Your Branch ")
+College = input("Your college Name ")
+Gender = input("Your Gender ")
+Age = input("Your Age ")
+
+print(Name + "\n" + Branch + "\n" + College + "\n" + Gender + "\n" + Age)
---
+++ Day1/Problem2.py
@@ -0,0 +1,5 @@
+import math
+
+a = int(input("Enter integer that is want to take"))
+
+print(math.sqrt(a))
